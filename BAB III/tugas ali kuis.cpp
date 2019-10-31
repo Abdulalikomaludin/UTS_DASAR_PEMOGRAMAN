39d7d08eb07f918d0f2b3f56744b8928b86e813d
Abdulalikomaludin <aliabdul0218@gmail.com>
2019-10-31 12:56:06 +0700

    UTS_DASAR_PEMOGRAMAN

---
+++ BAB III/tugas ali kuis.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char Nama[20], Alamat[20], Nim[20];
+
+    cout << "================================= \n"<<endl;
+    cout << " WILUJEUNG SUMPING DI PROGRAM C++ \n"<<endl;
+    cout << "             BIODATA              \n"<<endl;
+    cout << "================================= \n"<<endl;
+    cout <<endl;
+    cout << "Masukan Nama Anda         :";
+    cin  >> Nama;
+    cout << "Masukan Alamat            :";
+    cin  >> Alamat;
+    cout << "Masukan Nim               :";
+    cin  >> Nim;
+    cout << "            BIODATA      \n";
+    cout << " Nama                     :"<<Nama;
+    cout <<endl;
+    cout << " Alamat                   :"<<Alamat;
+    cout <<endl;
+    cout << " Nim                      :"<<Nim;
+    cout <<endl;
+return 0;
+}
